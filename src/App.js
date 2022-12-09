219a5e80066faf28156d8dec23311833354086cf
Julia Lima <julia.manvailer17@icloud.com>
2022-12-09 12:24:40 -0500

    started login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
-import '../CSS/App.css';
+import Login from './pages/Login';
+import './CSS/App.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
       
     </div>
   );
